56b1f1633c0879fa57655b068852a03ad5b1479a
Rashed Arman <rashedarman21@gmail.com>
2023-01-17 21:30:20 +0600

    Create static Calculator component & basic styles

--- src/components/Calculator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Calculator extends React.Component {
-  render() {
-    return <h1>Calculator</h1>;
-  }
-}
-
-export default Calculator;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './Calculator.css';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="calculator">
+          <input className="output" type="text" value="0" readOnly />
+          <button type="button" className="button">
+            AC
+          </button>
+          <button type="button" className="button">
+            +/-
+          </button>
+          <button type="button" className="button">
+            %
+          </button>
+          <button type="button" className="button secondary">
+            +
+          </button>
+          <button type="button" className="button">
+            7
+          </button>
+          <button type="button" className="button">
+            8
+          </button>
+          <button type="button" className="button">
+            9
+          </button>
+          <button type="button" className="button secondary">
+            x
+          </button>
+          <button type="button" className="button">
+            4
+          </button>
+          <button type="button" className="button">
+            5
+          </button>
+          <button type="button" className="button">
+            6
+          </button>
+          <button type="button" className="button secondary">
+            -
+          </button>
+          <button type="button" className="button">
+            1
+          </button>
+          <button type="button" className="button">
+            2
+          </button>
+          <button type="button" className="button">
+            3
+          </button>
+          <button type="button" className="button secondary">
+            +
+          </button>
+          <button type="button" className="button zero">
+            0
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button secondary">
+            =
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
