7d0d6c6cc2e51c795d25a6d59fe90d26f8d59366
Rashed Arman <rashedarman21@gmail.com>
2023-01-21 00:31:20 +0600

    Refactor Calculator component with hooks

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,172 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: null,
-      next: null,
-    };
-    this.calcEvent = this.calcEvent.bind(this);
-  }
+function Calculator() {
+  const [calcData, setCalcData] = useState({
+    total: 0,
+    operation: null,
+    next: null,
+  });
+  const { total, next, operation } = calcData;
 
-  calcEvent(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const calcEvent = (e) => {
+    setCalcData((state) => calculate(state, e.textContent));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
+  useEffect(() => {
+    document.querySelectorAll('button[type="button"]').forEach((button) => {
+      button.addEventListener('click', () => calcEvent(button));
+    });
+  }, []);
 
-    return (
-      <div className="container">
-        <div className="calculator">
-          <p className="output">
-            {total}
-            {operation}
-            {next}
-          </p>
+  return (
+    <div className="container">
+      <div className="calculator">
+        <p className="output">
+          {total}
+          {operation}
+          {next}
+        </p>
 
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            AC
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            +/-
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            %
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-secondary"
-          >
-            ÷
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            7
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            8
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            9
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-secondary"
-          >
-            x
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            4
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            5
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            6
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-secondary"
-          >
-            -
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            1
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            2
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            3
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-secondary"
-          >
-            +
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            id="zero-btn"
-            className="btn btn-light"
-          >
-            0
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-light"
-          >
-            .
-          </button>
-          <button
-            onClick={this.calcEvent}
-            type="button"
-            className="btn btn-secondary"
-          >
-            =
-          </button>
-        </div>
+        <button type="button" className="btn btn-light">
+          AC
+        </button>
+        <button type="button" className="btn btn-light">
+          +/-
+        </button>
+        <button type="button" className="btn btn-light">
+          %
+        </button>
+        <button type="button" className="btn btn-secondary">
+          ÷
+        </button>
+        <button type="button" className="btn btn-light">
+          7
+        </button>
+        <button type="button" className="btn btn-light">
+          8
+        </button>
+        <button type="button" className="btn btn-light">
+          9
+        </button>
+        <button type="button" className="btn btn-secondary">
+          x
+        </button>
+        <button type="button" className="btn btn-light">
+          4
+        </button>
+        <button type="button" className="btn btn-light">
+          5
+        </button>
+        <button type="button" className="btn btn-light">
+          6
+        </button>
+        <button type="button" className="btn btn-secondary">
+          -
+        </button>
+        <button type="button" className="btn btn-light">
+          1
+        </button>
+        <button type="button" className="btn btn-light">
+          2
+        </button>
+        <button type="button" className="btn btn-light">
+          3
+        </button>
+        <button type="button" className="btn btn-secondary">
+          +
+        </button>
+        <button type="button" id="zero-btn" className="btn btn-light">
+          0
+        </button>
+        <button type="button" className="btn btn-light">
+          .
+        </button>
+        <button type="button" className="btn btn-secondary">
+          =
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
