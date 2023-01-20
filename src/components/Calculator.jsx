6839ace50df6f9d2daf1a4229b114bfd345b8a07
Rashed Arman <rashedarman21@gmail.com>
2023-01-20 23:07:57 +0600

    Some CSS & visual improvements

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,61 +7,61 @@ class Calculator extends React.Component {
       <div className="container">
         <div className="calculator">
           <input className="output" type="text" value="0" readOnly />
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             AC
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             +/-
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             %
           </button>
-          <button type="button" className="button secondary">
+          <button type="button" className="btn btn-secondary">
             ÷
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             7
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             8
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             9
           </button>
-          <button type="button" className="button secondary">
+          <button type="button" className="btn btn-secondary">
             x
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             4
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             5
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             6
           </button>
-          <button type="button" className="button secondary">
+          <button type="button" className="btn btn-secondary">
             -
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             1
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             2
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             3
           </button>
-          <button type="button" className="button secondary">
+          <button type="button" className="btn btn-secondary">
             +
           </button>
-          <button type="button" className="button zero">
+          <button type="button" id="zero-btn" className="btn btn-light">
             0
           </button>
-          <button type="button" className="button">
+          <button type="button" className="btn btn-light">
             .
           </button>
-          <button type="button" className="button secondary">
+          <button type="button" className="btn btn-secondary">
             =
           </button>
         </div>
