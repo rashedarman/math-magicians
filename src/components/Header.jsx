e1d972854cf45666099eb01ee37c2d389fc1d1db
Rashed Arman <rashedarman21@gmail.com>
2023-01-27 20:57:20 +0600

    Fix: header elements collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,23 @@ import './Header.module.scss';
 class Header extends React.Component {
   render() {
     return (
-      <header className="container">
-        <nav>
-          <h1>Math Magicians</h1>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/calculator">Calculator</a>
-            </li>
-            <li>
-              <a href="/quote">Quote</a>
-            </li>
-          </ul>
-        </nav>
+      <header>
+        <div className="container">
+          <nav>
+            <h1>Math Magicians</h1>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/calculator">Calculator</a>
+              </li>
+              <li>
+                <a href="/quote">Quote</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
