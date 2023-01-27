f9c37d20f9267cb28f8bc3413688610f950ad2d1
Rashed Arman <rashedarman21@gmail.com>
2023-01-27 22:35:56 +0600

    Match calculator page wireframe

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Calculator from '../components/Calculator';
 import PageWrapper from '../components/PageWrapper';
 
 class CalculatorPage extends React.Component {
   render() {
     return (
       <PageWrapper>
-        <h1>Calculator</h1>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <h1>Let&apos;s do some math!</h1>
+          <Calculator />
+        </div>
       </PageWrapper>
     );
   }
