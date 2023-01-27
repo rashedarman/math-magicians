0d4be643f43c3d4452520e641492482ec7fc7411
Rashed Arman <rashedarman21@gmail.com>
2023-01-27 22:23:23 +0600

    Match homepage wireframe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,28 @@ class HomePage extends React.Component {
   render() {
     return (
       <PageWrapper>
-        <h1>Home</h1>
+        <h1 style={{ marginBottom: '3rem' }}>Welcome to our page!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          dolore atque illo perspiciatis animi velit iure consequatur amet
+          quisquam veniam neque modi, alias nesciunt expedita et. Aspernatur
+          alias debitis, adipisci omnis ratione, illum soluta voluptatem tempore
+          eius laborum incidunt cupiditate neque aliquid sunt magnam nam porro
+          enim est totam placeat vel voluptatum voluptate molestias! Ab esse
+          possimus vitae maxime assumenda!
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eaque
+          repudiandae autem inventore quam voluptate quasi perspiciatis. Nam
+          eveniet expedita laborum optio dolorem. Ea facilis, aliquid veritatis
+          dignissimos placeat voluptates pariatur repellendus eligendi
+          necessitatibus officia nostrum quibusdam nihil nulla et sunt quo
+          aspernatur earum veniam laborum eaque! Dicta eum dignissimos eveniet.
+          Unde laboriosam, esse a animi distinctio sunt eius, saepe recusandae
+          ut natus eligendi asperiores magni aut amet cumque vitae repellat!
+          Tempora architecto nam obcaecati alias corporis cum praesentium est.
+        </p>
       </PageWrapper>
     );
   }
