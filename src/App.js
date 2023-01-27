0cbbe0e4433404896589829bdf3a64f3160f21a3
Rashed Arman <rashedarman21@gmail.com>
2023-01-27 22:05:07 +0600

    Install and implement React Router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CalculatorPage from './pages/Calculator';
+import HomePage from './pages/Home';
+import QuotePage from './pages/Quote';
+
 import './App.scss';
-import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
+          <Route path="/quote" element={<QuotePage />} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
